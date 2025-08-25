e514f0a83a9f2fec8651ea90cfc0b1649a3f2bab
Eli Stone <2546188+elistone@users.noreply.github.com>
2025-08-25 21:15:41 +0100

    add GitHub Actions workflow for deployment and configure base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const isProd = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
+    base: isProd ? '/standups/' : '/',
     plugins: [vue()],
     build: {
         sourcemap: true
